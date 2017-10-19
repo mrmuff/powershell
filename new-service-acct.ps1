d72c98a657856bb53e8fc7bfd0b2b53f6b79f5f5
Erter <mrmuff@icloud.com>
2017-10-19 11:44:08 -0700

    Updated new service account script so it write the userprincipalname attribute.

--- new-service-acct.ps1
+++ new-service-acct.ps1
@@ -1,15 +1,18 @@
 function new-service-acct
 {
-
     $PlainPassword = $args[1]
     $SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force
 
+    [string]$name = $args[0]
 
-    New-ADUser -Server fakeserver -Name $args[0] `
-    -Path "OU=FakeOU,OU=JunkOU,DC=FakeDomain,DC=com" `
+    New-ADUser -Server fakeservername `
+    -Name $args[0] `
+    -UserPrincipalName "$name@fakedomainname.com" `
+    -Path "OU=fakeOU02,OU=fakeOU01,DC=fakedomainname,DC=com" `
     -PasswordNeverExpires:$true `
     -AccountPassword $SecurePassword `
-    -Enabled:$true
+    -Enabled:$true `
+    -verbose
 }
 
-new-service-acct junkAccountName fakeJunkPassword
+new-service-acct "junkuser" "fakepassword"
